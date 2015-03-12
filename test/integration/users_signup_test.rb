e13aced61e03ee761f205bc2d210faa22cc95ca1
Jacob Swartzentruber <jsswart@gmail.com>
2015-03-12 15:10:54 +0000

    Fixed user signup test so it works

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   
   test "invalid signup information" do
-    get sign_up path
+    get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: {name: "",
                               email: "user@invalid",
@@ -14,7 +14,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
   
   test "valid signup information" do
-    get sign_up path
+    get signup_path
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: {name: "Example User",
                                            email: "user@example.com",
